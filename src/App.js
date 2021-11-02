7cd79fbf76ef5e9fbb4242abfa5636ff105f3192
rshubham005 <rathor.sr143.sr@gmail.com>
2021-11-02 22:32:02 +0530

    first comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState} from 'react';
+import del from './Image/del.png'
+import './App.scss';
 function App() {
+  let [task,settask]=useState()
+  let [list,setlist]=useState([])
+  let holdtask = (e)=>
+  {
+    settask(e.target.value)
+  };
+  let addtask=()=>
+  {
+    if(task!=="")
+    {
+    setlist([...list,task])
+    document.getElementById('inp').value=""
+    }
+    settask("")
+  }
+  let taskdone=(e)=>
+  {
+    let id=e.target.id;
+    let res = list.filter(item => item !==list[id])
+    setlist([...res])
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todo">
+        <h1>To-Do List</h1>
+        <input type="text" id="inp" onChange={holdtask}/>
+        <button type="submit" onClick={addtask}>Submit</button>
+        <ul>
+        {
+          list.map((item,index)=>
+            {
+              return(<li key={index}>
+                <p>{item}</p>
+                <img src={del} id={index} onClick={taskdone} alt="check button"/>
+              </li>)
+            })
+        }
+        </ul>
+      </div>
     </div>
   );
 }
